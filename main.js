d490568f9f88d354fb8d72afa1a1e457ea32bf5e
1991Pawel <43860287+1991Pawel@users.noreply.github.com>
2020-02-19 11:20:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const cart = [];
 
-const displayProductInDom = (productsItems,querySelector) => {
-  if(productsItems.length < 0) return;
+const displayProductInDom = (productsItems, querySelector) => {
+  if (productsItems.length < 0) return;
 
   const productContainer = document.querySelector(querySelector);
   productContainer.innerHTML = "";
 
   productsItems.forEach(item => {
-    let product = document.createElement('div');
- 
-  if(productsItems === cart){
-    product.innerHTML = `<div id="${item.id}" class="cart__item">
+    let product = document.createElement("div");
+
+    if (productsItems === cart) {
+      product.innerHTML = `<div id="${item.id}" class="cart__item">
     <img class="cart__item__image" src="${item.srcImg}" alt="fafa">
     <h3 class="cart__item__name">${item.name}</h3>
     <h3 class="cart__item__price">${item.price}</h3>
@@ -18,61 +18,62 @@ const displayProductInDom = (productsItems,querySelector) => {
     <h3 class="cart__item__quantity">1</h3>
     <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
     <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
-  </div>`
-    
-  }else if (productsItems === products){
-    product.innerHTML = `<div id="${item.id}" class="product">
+  </div>`;
+    } else if (productsItems === products) {
+      product.innerHTML = `<div id="${item.id}" class="product">
         <img class="product__image" src="${item.srcImg}">
         <h2 class="product__name">${item.name}</h2>
         <h3 class="product__price">${item.price}</h3>
         <button class="btn btn--primary" data-action="ADD_TO_CART">Add To Cart</button>
-        </div>` 
-        console.log('wszedlem')
-    };
+        </div>`;
+      console.log("wszedlem");
+    }
     productContainer.appendChild(product);
   });
 };
 
-displayProductInDom(products,'.products');
-displayProductInDom(cart, '.cart');
+displayProductInDom(products, ".products");
+displayProductInDom(cart, ".cart");
 
 listnersForButtons = () => {
-  const AddToCartButtons = document.querySelectorAll('[data-action="ADD_TO_CART"]');
-  const RemoveFromCartButtons = document.querySelectorAll('[data-action="REMOVE_ITEM"]');
+  const AddToCartButtons = document.querySelectorAll(
+    '[data-action="ADD_TO_CART"]'
+  );
+  const RemoveFromCartButtons = document.querySelectorAll(
+    '[data-action="REMOVE_ITEM"]'
+  );
 
-  RemoveFromCartButtons.forEach((btn) => btn.addEventListener('click', RemoveFromCartItems));
-  AddToCartButtons.forEach((btn) => btn.addEventListener('click', AddToCartItems));  
-}
+  RemoveFromCartButtons.forEach(btn =>
+    btn.addEventListener("click", RemoveFromCartItems)
+  );
+  AddToCartButtons.forEach(btn =>
+    btn.addEventListener("click", AddToCartItems)
+  );
+};
 
 const getProductId = e => Number(e.target.parentNode.id);
 
-
-  const AddToCartItems = (e) => {
+const AddToCartItems = e => {
   const productId = getProductId(e);
   const isAllreadyInCart = cart.some(item => item.id === productId);
-  if(isAllreadyInCart) return;
-  
+  if (isAllreadyInCart) return;
+
   products.map(item => {
-        if (item.id === productId) {
-          cart.push(item);
-        }
-      });
+    if (item.id === productId) {
+      cart.push(item);
+    }
+  });
 
-  displayProductInDom(cart, '.cart');
+  displayProductInDom(cart, ".cart");
   listnersForButtons();
+};
 
-}
-
-const RemoveFromCartItems = (e) => {
+const RemoveFromCartItems = e => {
   const productId = getProductId(e);
   const itemIndex = cart.findIndex(item => item.id == productId);
   cart.splice(itemIndex, 1);
-  displayProductInDom(cart, '.cart');
-    listnersForButtons();
-}
-
-
+  displayProductInDom(cart, ".cart");
+  listnersForButtons();
+};
 
 listnersForButtons();
-
-
