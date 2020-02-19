9e6f41c4d452b3e8a44a1fe4b44e3818bfb20c79
1991Pawel <43860287+1991Pawel@users.noreply.github.com>
2020-02-19 11:19:47 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,101 +1,78 @@
 const cart = [];
 
-const displayProductInDom = products => {
-  const productContainer = document.querySelector(".products");
+const displayProductInDom = (productsItems,querySelector) => {
+  if(productsItems.length < 0) return;
+
+  const productContainer = document.querySelector(querySelector);
   productContainer.innerHTML = "";
-  products.forEach(item => {
-    const itemHtml = `<div id="${item.id}" class="product">
+
+  productsItems.forEach(item => {
+    let product = document.createElement('div');
+ 
+  if(productsItems === cart){
+    product.innerHTML = `<div id="${item.id}" class="cart__item">
+    <img class="cart__item__image" src="${item.srcImg}" alt="fafa">
+    <h3 class="cart__item__name">${item.name}</h3>
+    <h3 class="cart__item__price">${item.price}</h3>
+    <button class="btn btn--primary btn--small" data-action="DECREASE_ITEM">&minus;</button>
+    <h3 class="cart__item__quantity">1</h3>
+    <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
+    <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
+  </div>`
+    
+  }else if (productsItems === products){
+    product.innerHTML = `<div id="${item.id}" class="product">
         <img class="product__image" src="${item.srcImg}">
         <h2 class="product__name">${item.name}</h2>
         <h3 class="product__price">${item.price}</h3>
         <button class="btn btn--primary" data-action="ADD_TO_CART">Add To Cart</button>
-    </div>`;
-    productContainer.innerHTML += itemHtml;
+        </div>` 
+        console.log('wszedlem')
+    };
+    productContainer.appendChild(product);
   });
 };
 
-const listnersForAddButtons = () => {
-  const AddToCartButtons = document.querySelectorAll(
-    '[data-action="ADD_TO_CART"]'
-  );
-  AddToCartButtons.forEach(btn => {
-    btn.addEventListener("click", clickedIdProduct);
-  });
-};
+displayProductInDom(products,'.products');
+displayProductInDom(cart, '.cart');
 
-const clickedIdProduct = e => {
-  const productId = Number(e.target.parentNode.id);
-  addProductToCart(productId);
+listnersForButtons = () => {
+  const AddToCartButtons = document.querySelectorAll('[data-action="ADD_TO_CART"]');
+  const RemoveFromCartButtons = document.querySelectorAll('[data-action="REMOVE_ITEM"]');
 
-  cart.forEach(item => {
-    if (item.id !== productId) {
-      console.log("zmien na disable false");
-      e.target.setAttribute("disabled", false);
-      e.target.textContent = "In Cart";
-    } else {
-      console.log("zmien na disable true");
-      e.target.setAttribute("disabled", true);
-      e.target.textContent = "Add To Cart";
-    }
-  });
-};
+  RemoveFromCartButtons.forEach((btn) => btn.addEventListener('click', RemoveFromCartItems));
+  AddToCartButtons.forEach((btn) => btn.addEventListener('click', AddToCartItems));  
+}
 
-const addProductToCart = productId => {
+const getProductId = e => Number(e.target.parentNode.id);
+
+
+  const AddToCartItems = (e) => {
+  const productId = getProductId(e);
   const isAllreadyInCart = cart.some(item => item.id === productId);
-  if (isAllreadyInCart) return;
-
+  if(isAllreadyInCart) return;
+  
   products.map(item => {
-    if (item.id === productId) {
-      cart.push(item);
-    }
-  });
-  showCartItemsInDom(cart);
-};
+        if (item.id === productId) {
+          cart.push(item);
+        }
+      });
 
-const listnersForRemoveButtons = () => {
-  const removeFromCartButtons = document.querySelectorAll(
-    '[data-action="REMOVE_ITEM"]'
-  );
+  displayProductInDom(cart, '.cart');
+  listnersForButtons();
 
-  removeFromCartButtons.forEach(btn =>
-    btn.addEventListener("click", removeCartItem)
-  );
-};
+}
 
-const removeCartItem = e => {
-  const removeItem = e.target.parentNode;
-  DomElement = document.getElementById(removeItem.id);
-
-  const button = (DomElement.querySelector("button").disabled = false);
-  button.textContent = "Add To Cart";
-
-  removeItem.remove();
-  const productId = Number(e.target.parentNode.id);
+const RemoveFromCartItems = (e) => {
+  const productId = getProductId(e);
   const itemIndex = cart.findIndex(item => item.id == productId);
   cart.splice(itemIndex, 1);
-};
+  displayProductInDom(cart, '.cart');
+    listnersForButtons();
+}
 
-showCartItemsInDom = cart => {
-  if (cart.length) {
-    const cartContainer = document.querySelector(".cart");
-    cartContainer.innerHTML = "";
-    cart.forEach(item => {
-      const cartItem = document.createElement("div");
-      cartItem.classList.add("cart__item");
-      cartItem.id = item.id;
-      cartItem.innerHTML = `<img class="cart__item__image" src="${item.srcImg}" alt="fafa">
-        <h3 class="cart__item__name">${item.name}</h3>
-        <h3 class="cart__item__price">${item.price}</h3>
-        <button class="btn btn--primary btn--small" data-action="DECREASE_ITEM">&minus;</button>
-        <h3 class="cart__item__quantity">1</h3>
-        <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
-        <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>`;
-      cartContainer.appendChild(cartItem);
-    });
-    listnersForRemoveButtons();
-  }
-};
 
-showCartItemsInDom(cart);
-displayProductInDom(products);
-listnersForAddButtons();
+
+listnersForButtons();
+
+
