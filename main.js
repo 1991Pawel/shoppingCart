c046181ba5c90b3c0447594b8ecf9ae0cc8f55db
1991Pawel <43860287+1991Pawel@users.noreply.github.com>
2020-02-20 13:25:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const cart = [];
+let cart = [];
+if (JSON.parse(localStorage.getItem("cart")) !== null) {
+  cart = JSON.parse(localStorage.getItem("cart"));
+}
 
 const getProductId = e => Number(e.target.parentNode.id);
 const isAllreadyInCart = productId => cart.some(item => item.id === productId);
@@ -51,7 +54,7 @@ const renderCartDom = () => {
      <h3 class="cart__item__name">${item.name}</h3>
       <h3 class="cart__item__price">${item.price}</h3>
       <button class="btn btn--primary btn--small" data-action="DECREASE_ITEM">&minus;</button>
-      <h3 class="cart__item__quantity">1</h3>
+      <h3 class="cart__item__quantity">${item.quantity ? item.quantity : 1}</h3>
       <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
       <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
       </div>`;
@@ -67,22 +70,73 @@ const listnersForButtons = () => {
     '[data-action="REMOVE_ITEM"]'
   );
 
+  const increaseQuantityButton = document.querySelectorAll(
+    '[data-action="INCREASE_ITEM"]'
+  );
+  const decreseQuantityButton = document.querySelectorAll(
+    '[data-action="DECREASE_ITEM"]'
+  );
+
+  increaseQuantityButton.forEach(btn => {
+    btn.addEventListener("click", updateQuantity);
+  });
+
+  decreseQuantityButton.forEach(btn => {
+    btn.addEventListener("click", updateQuantity);
+  });
+
   RemoveFromCartButtons.forEach(btn =>
     btn.addEventListener("click", RemoveFromCartItems)
   );
+
   AddToCartButtons.forEach(btn =>
     btn.addEventListener("click", AddToCartItems)
   );
 };
 
-const changeButtonStatusInDom = productId => {
-  const button = document.querySelector(`[id='${productId}'] button`);
-  if (isAllreadyInCart(productId)) {
-    button.textContent = "In Cart";
-    button.setAttribute("disabled", true);
+const updateQuantity = e => {
+  const handleButtons = e.target.dataset.action;
+  const productId = getProductId(e);
+  const itemIndex = cart.findIndex(item => item.id == productId);
+  const quantity = document.querySelector(
+    `[id='${productId}'] .cart__item__quantity`
+  );
+
+  if (handleButtons === "DECREASE_ITEM") {
+    if (quantity.textContent <= 1) {
+      RemoveFromCartItems(e);
+      return;
+    }
+
+    quantity.textContent--;
+    let storeData = JSON.parse(localStorage.getItem("cart"));
+    storeData[itemIndex].quantity = quantity.textContent;
+    localStorage.setItem("cart", JSON.stringify(storeData));
+  } else if (handleButtons === "INCREASE_ITEM") {
+    quantity.textContent++;
+
+    let storeData = JSON.parse(localStorage.getItem("cart"));
+    storeData[itemIndex].quantity = quantity.textContent;
+    localStorage.setItem("cart", JSON.stringify(storeData));
+  }
+};
+
+const changeButtonStatusInDom = (productId = null) => {
+  if (productId === null) {
+    cart.forEach(item => {
+      const button = document.querySelector(`[id='${item.id}'] button`);
+      button.setAttribute("disabled", true);
+      button.textContent = "In Cart";
+    });
   } else {
-    button.textContent = "Add To Cart";
-    button.removeAttribute("disabled");
+    const button = document.querySelector(`[id='${productId}'] button`);
+    if (isAllreadyInCart(productId)) {
+      button.textContent = "In Cart";
+      button.setAttribute("disabled", true);
+    } else {
+      button.textContent = "Add To Cart";
+      button.removeAttribute("disabled");
+    }
   }
 };
 
@@ -94,6 +148,7 @@ const AddToCartItems = e => {
   products.forEach(item => {
     if (item.id === productId) {
       cart.push(item);
+      localStorage.setItem("cart", JSON.stringify(cart));
     }
   });
 
@@ -106,15 +161,25 @@ const AddToCartItems = e => {
 const RemoveFromCartItems = e => {
   const productId = getProductId(e);
   const itemIndex = cart.findIndex(item => item.id == productId);
+
+  // local storange
+  const storeData = JSON.parse(localStorage.getItem("cart"));
+  storeData.splice(itemIndex, 1);
+  localStorage.setItem("cart", JSON.stringify(storeData));
+  //
+
   cart.splice(itemIndex, 1);
+
   changeButtonStatusInDom(productId);
   renderCartDom();
   listnersForButtons();
   renderTotalCartPriceDom();
 };
 
-window.onload = function() {
+window.onload = function(e) {
   renderProductDom(products);
   renderCartDom();
   listnersForButtons();
+  changeButtonStatusInDom();
+  renderTotalCartPriceDom();
 };
